89a0bf2d826063928c14fa00b73cf5df03469dae
Keenan Lidral-Porter <klp2012@gmail.com>
2014-09-05 14:13:50 -0700

    (feat) Add placeMarkers function, works with fake data

--- client/app/scripts/controllers.js
+++ client/app/scripts/controllers.js
@@ -8,6 +8,18 @@ angular.module('Client.controllers', [])
     $scope.centerOnMe(true, 'splash');
   };
 
+  // helper function to place markers (move into utility file/service eventually?)
+  $scope.placeMarkers = function(businesses){
+    businesses.forEach(function(business){
+      var markerPosition = new google.maps.LatLng(business.latitude, business.longitude);
+      var marker = new google.maps.Marker({
+        position: markerPosition,
+        map: $scope.map,
+        title: business.businessName
+      });
+    });
+  };
+
   $scope.centerOnMe = function (showOrHideBackdrop, templateName) {
     console.log('Centering');
     if (!$scope.map) {
@@ -24,11 +36,8 @@ angular.module('Client.controllers', [])
       console.log('Got pos', pos);
       var MyPosition = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
       $scope.map.setCenter(MyPosition);
-      $scope.markers = (new google.maps.Marker({
-        position: MyPosition,
-        map: $scope.map,
-        title: 'test'
-      }));
+
+      $scope.placeMarkers([{businessName: "coffee", latitude: 37.7841, longitude:-122.410}]);
 
       $ionicLoading.hide();
     }, function (error) {
