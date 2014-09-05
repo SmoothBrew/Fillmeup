ad4ac58df5781d6280d4fe0223289491f663f7e0
Wil A <andradewilker@gmail.com>
2014-09-05 11:49:19 -0700

    Added log in button and ng-controller to handle log in

--- client/app/scripts/controllers.js
+++ client/app/scripts/controllers.js
@@ -28,4 +28,9 @@ angular.module('Client.controllers', [])
       alert('Unable to get location: ' + error.message);
     });
   };
+
+  $scope.logIn = function(){
+    console.log('Logging in...');
+  };
+
 });
