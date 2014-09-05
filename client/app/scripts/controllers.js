e69b42e33d22f5b7800a23c2fa4ae45e8ba9a0d1
Andrew Switlyk <andrewcswitlyk@gmail.com>
2014-09-04 22:07:21 -0700

    (feat) - add reverse geo-coding to obtain address from centering latitude and longitude

--- client/app/scripts/controllers.js
+++ client/app/scripts/controllers.js
@@ -3,6 +3,9 @@
 angular.module('Client.controllers', [])
 
 .controller('MapCtrl', function($scope, $ionicLoading) {
+  // create a google maps geocoder
+  var geocoder = new google.maps.Geocoder();
+
   $scope.mapCreated = function(map) {
     $scope.map = map;
     $scope.centerOnMe(true, 'splash');
@@ -22,7 +25,25 @@ angular.module('Client.controllers', [])
 
     navigator.geolocation.getCurrentPosition(function (pos) {
       console.log('Got pos', pos);
+      // get latitude and longitude position
+      var lat = pos.coords.latitude;
+      var lng = pos.coords.longitude;
+      // // reverse geo-code this position into an address
+      // // to use in GET request to server (for yelp API)
+      var latlng = new google.maps.LatLng(lat, lng);
+      geocoder.geocode({'latLng': latlng}, function(results, status) {
+        console.log('geocoder results are: ', results);
+        console.log('geocoder status is: ', status);
+        if( status == google.maps.GeocoderStatus.OK) {
+          console.log('geocoding ok');
+          if(results[1]) {
+            console.log('reverse geocode results are: ', results);
+          }
+        }
+      });
+
       $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+
       $ionicLoading.hide();
     }, function (error) {
       alert('Unable to get location: ' + error.message);
